63807eb2309c019af92cdc84a5846bad3d587af1
Shevchenko Alexander <loki.sup@gmail.com>
2015-02-12 18:18:51 +0200

    [Blog-project]
    - implemented create method;

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,15 @@ class ArticlesController < ApplicationController
   def new
 
   end
+
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
