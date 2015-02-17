e4a76b1ab01d2fe6c29a625736c58b91877b7703
Shevchenko Alexander <loki.sup@gmail.com>
2015-02-17 16:59:08 +0200

    [Blog-project]
    - added list of articles representations, nav links;

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,14 @@
 class ArticlesController < ApplicationController
-  def new
 
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
   end
 
   def create
